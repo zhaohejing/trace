c76b661ec3c52c365214cfc7d40699a7a175fe38
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-08-01 11:16:15 +0800

    阿瓦达

--- src/Trace/views/postage/index.js
+++ src/Trace/views/postage/index.js
@@ -9,17 +9,11 @@
                         App.initAjax();
                     });
                 var vm = this;
-                // ajax初始化
-                $scope.$on('$viewContentLoaded',
-                    function () {
-                        App.initAjax();
-                    });
-                var vm = this;
                 vm.filter = { pageNum: 1, pageSize: 10, name: "" };
 
                 //页面属性
                 vm.table = {
-                    data: [], //数据集
+                    rows: [], //数据集
                     pageConfig: { //分页配置
                         currentPage: 1, //当前页
                         itemsPerPage: 10, //页容量
@@ -30,11 +24,11 @@
                 vm.init = function () {
                     vm.filter.pageNum = vm.table.pageConfig.currentPage;
                     vm.filter.pageSize = vm.table.pageConfig.itemsPerPage;
-                    dataFactory.action("api/sysrole/getAll", "", null, vm.filter)
+                    dataFactory.action("api/post/list", "", null, vm.filter)
                         .then(function (res) {
                             if (res.success) {
                                 vm.table.pageConfig.totalItems = res.total;
-                                vm.table.data = res.result;
+                                vm.table.rows = res.result;
                                 vm.table.pageConfig.onChange = function () {
                                     vm.init();
                                 }
@@ -46,10 +40,10 @@
                 vm.init();
                 vm.add = function () {
                     var modal = $uibModal.open({
-                        templateUrl: 'views/manager/roles/modal.html',
-                        controller: 'views.manager.roles.modal as vm',
+                        templateUrl: 'views/postage/modal.html',
+                        controller: 'views.postage.modal as vm',
                         backdrop: 'static',
-                        size: 'lg', //模态框的大小尺寸
+                       // size: 'lg', //模态框的大小尺寸
                         resolve: {
                             model: function () { return {} }
                         }
@@ -60,8 +54,8 @@
                 }
                 vm.edit = function (row) {
                     var modal = $uibModal.open({
-                        templateUrl: 'views/manager/roles/modal.html',
-                        controller: 'views.manager.roles.modal as vm',
+                        templateUrl: 'views/postage/modal.html',
+                        controller: 'views.postage.modal as vm',
                         backdrop: 'static',
                         size: 'lg', //模态框的大小尺寸
                         resolve: {
@@ -72,27 +66,14 @@
                         vm.init();
                     });
                 }
-                vm.allow = function (row) {
-                    var modal = $uibModal.open({
-                        templateUrl: 'views/common/allowpermission.html',
-                        controller: 'views.common.allowpermission as vm',
-                        backdrop: 'static',
-                        //  size: 'lg', //模态框的大小尺寸
-                        resolve: {
-                            model: function () { return { id: row.id } }
-                        }
-                    });
-                    modal.result.then(function (response) {
-                        vm.init();
-                    });
-                }
+              
                 vm.delete = function (row) {
                     abp.message.confirm(
                         '删除将导致数据无法显示', //确认提示
                         '确定要删除么?', //确认提示（可选参数）
                         function (isConfirmed) {
                             if (isConfirmed) {
-                                dataFactory.action("api/sysrole/delete", "", null, { list: [row.id] })
+                                dataFactory.action("api/post/delete", "", null, { list: [row.id] })
                                     .then(function (res) {
                                         abp.notify.success("删除成功");
                                         vm.init();
--- src/Trace/views/postage/modal.js
+++ src/Trace/views/postage/modal.js
@@ -1,29 +1,19 @@
 ﻿angular.module('MetronicApp').controller('views.onlinemall.figure.modal',
-    ['$scope', 'settings', '$uibModalInstance', 'model', 'dataFactory', '$qupload',
-        function ($scope, settings, $uibModalInstance, model, dataFactory, $qupload) {
+    ['$scope', 'settings', '$uibModalInstance', 'model', 'dataFactory',
+        function ($scope, settings, $uibModalInstance, model, dataFactory) {
             $scope.$on('$viewContentLoaded', function () {
                 App.initAjax();
 
             });
             var vm = this;
-            vm.model = {};
-            vm.url = "api/gift/modify";
+            vm.post = {};
+            vm.url = "api/post/add";
             vm.save = function () {
-                if (!vm.gift.activityId || vm.gift.activityId <= 0) {
-                    abp.notify.warn("请先创建活动");
-                    return;
-                }
-                if (vm.file.show.length != 1) {
-                    abp.notify.warn("请先上传文件");
-                    return;
-                }
-                vm.gift.imageName = vm.file.show[0].imageName;
-                vm.gift.imageUrl = vm.file.show[0].imageUrl;
-                dataFactory.action(vm.url, "", null, vm.gift).then(function (res) {
+                dataFactory.action(vm.url, "", null, vm.post).then(function (res) {
                     if (res.success) {
                         $uibModalInstance.close();
                     } else {
-                        abp.notify.error("保存失败,请重试");
+                        abp.notify.error(res.error);
                     }
                 });
             };
@@ -32,9 +22,10 @@
             };
             vm.init = function () {
                 if (model.id) {
-                    dataFactory.action("api/gift/detail", "", null, { id: model.id }).then(function (res) {
+                    vm.url = "api/post/update";
+                    dataFactory.action("api/post/get?id="+model.id, "GET", null, { }).then(function (res) {
                         if (res.success) {
-                            vm.model = res.result;
+                            vm.post = res.result;
                         } else {
                             abp.notify.error("获取失败,请重试");
                         }
@@ -42,53 +33,4 @@
                 }
             }
             vm.init();
-            vm.file = {
-                multiple: false,
-                token: abp.qiniuToken,
-                uploadstate: false,
-                show: [],
-                selectFiles: [],
-                start: function (index) {
-                    vm.file.selectFiles[index].progress = {
-                        p: 0
-                    };
-                    vm.file.selectFiles[index].upload = $qupload.upload({
-                        key: '',
-                        file: vm.file.selectFiles[index].file,
-                        token: vm.file.token
-                    });
-                    vm.file.selectFiles[index].upload.then(function (response) {
-                        var fileName = vm.file.selectFiles[index].file.name;
-                        var dto = {
-                            sort: index,
-                            title: fileName,
-                            url: abp.qiniuUrl + response.key,
-                            isTitle: fileName.indexOf("title") >= 0,
-                            isShare: fileName.indexOf("share") >= 0
-                        };
-                        vm.file.show.push(dto);
-                        vm.file.uploadstate = true;
-                    }, function (response) {
-                        abp.notify.error("上传失败,请重试");
-                    }, function (evt) {
-                        // progress
-                        vm.file.selectFiles[index].progress.p = Math.floor(100 * evt.loaded / evt.totalSize);
-                    });
-                },
-                abort: function () {
-                    //  vm.model.address = response.address;
-                    vm.file.show = [];
-                    vm.file.selectFiles = [];
-                },
-                onFileSelect: function ($files) {
-                    vm.file.selectFiles = [];
-                    var offsetx = vm.file.selectFiles.length;
-                    for (var i = 0; i < $files.length; i++) {
-                        vm.file.selectFiles[i + offsetx] = {
-                            file: $files[i]
-                        };
-                        vm.file.start(i + offsetx);
-                    }
-                }
-            }
         }]);
